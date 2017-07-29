d465797395ecc16b7611a4336a18c20abcfc4f2a
aakashhacker <aakashofficial143@gmail.com>
2017-07-29 22:09:55 +0530

    AlertBox implemented Baground image

--- app/src/main/java/com/example/ganesh/justjava/MainActivity.java
+++ app/src/main/java/com/example/ganesh/justjava/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.ganesh.justjava;
 
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,11 +22,13 @@ import static android.R.attr.value;
 public class MainActivity extends AppCompatActivity {
 
     int quantity = 0;
-
+    TextView quantityTextview,orderSummaryTextView;
+    EditText nameField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.FIRST_APPLICATION_WINDOW);
     }
 
     public void increment(View view) {
@@ -44,21 +50,45 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
-        EditText nameField = (EditText) findViewById(R.id.name_field);
+        final EditText nameField = (EditText) findViewById(R.id.name_field);
         String name = nameField.getText().toString();
         Log.v("MainActivity", "Name :" + name);
 
-        CheckBox whippedCreamCheckbox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        final CheckBox whippedCreamCheckbox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         boolean hasWhippedCream = whippedCreamCheckbox.isChecked();
         Log.v("MainActivity", "Has Whipped Cream :" + hasWhippedCream);
 
-        CheckBox choclateCheckbox = (CheckBox) findViewById(R.id.choclate_checkbox);
+        final CheckBox choclateCheckbox = (CheckBox) findViewById(R.id.choclate_checkbox);
         boolean haschoclate = choclateCheckbox.isChecked();
         Log.v("MainActivity", "Has Choclate :" + haschoclate);
 
-        int price = calculatePrice(hasWhippedCream, haschoclate);
+        final int price = calculatePrice(hasWhippedCream, haschoclate);
         String priceMessage = createOrderSummary(name, price, hasWhippedCream, haschoclate);
         displayMessage(priceMessage);
+
+        AlertDialog.Builder alertdialogbox = new AlertDialog.Builder(this);
+        alertdialogbox.setTitle("Final Order");
+
+        alertdialogbox.setMessage("Detail About Customer");
+        alertdialogbox.setCancelable(false);
+        alertdialogbox.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                nameField.setText("");
+                orderSummaryTextView.setText("$0");
+                quantityTextview.setText("0");
+
+            }
+        });
+        alertdialogbox.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                MainActivity.this.finish();
+            }
+        });
+        AlertDialog alertDialog = alertdialogbox.create();
+        alertDialog.show();
+
     }
 
     private int calculatePrice(boolean addWhippedCream, boolean addChoclate) {
@@ -85,13 +115,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void displayQuantity(int numberOfCOffees) {
-        TextView quantityTextview = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextview = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextview.setText("" + numberOfCOffees);
     }
 
     private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+         orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
         orderSummaryTextView.setText(message);
+    }
+
+    public void hideSoftKeyboard() {
+        if (getCurrentFocus() != null) {
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
     }
 
 }
